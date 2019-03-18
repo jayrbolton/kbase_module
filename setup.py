92b8609d94463ef6c6ab69a0cf547e076fa29de9
Jay R Bolton <jayrbolton@gmail.com>
2019-03-18 15:12:18 -0700

    0.0.1 -> 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='kbase_module',
-    version='0.0.1',
+    version='0.0.2',
     description='KBase SDK module utilities',
     author='KBase Team',
     author_email='info@kbase.us',
